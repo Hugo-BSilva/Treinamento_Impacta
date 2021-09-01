4b370074491f288f9872d5a64e9ee8523a8b1764
Hugo Barbosa da Silva <09silvahugo@gmail.com>
2021-09-01 17:52:58 -0300

    Estrutura_Repeticao

--- Aula-01/HelloWorld/HelloWorld/EstruturaDeRepeticao.cs
+++ Aula-01/HelloWorld/HelloWorld/EstruturaDeRepeticao.cs
@@ -11,8 +11,29 @@ namespace HelloWorld
             for(var i = 0; i < args.Length; i++)
             {
                 var arg = args[i];
-                Console.WriteLine($"Argumento: {arg}");
+                Console.WriteLine($"Argumento lido: {arg}");
             }
+
+            foreach (var item in args)
+            {
+                Console.WriteLine($"Argumento lido no foreach: {item}");
+            }
+
+            var loops2 = 0;
+            while (loops2 < args.Length)
+            {
+                var argumento = args[loops2];
+                Console.WriteLine($"Argumento lido no while: {argumento}");
+                loops2++;
+            }
+
+            var loops3 = 0;
+            do
+            {
+                var argumento = args[loops3];
+                Console.WriteLine($"Argumento lido no dowhile: {argumento}");
+                loops3++;
+            } while (loops3 < args.Length);
         }
     }
 }
