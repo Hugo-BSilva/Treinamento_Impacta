5315571c8833668330b6a56da867eba2427751b8
Hugo Barbosa da Silva <09silvahugo@gmail.com>
2021-09-02 17:42:29 -0300

    iniciando poo

---
+++ Aula-02-POO/EntendendoPOO/EntendendoPOO/Produto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntendendoPOO
+{
+    class Produto
+    {
+        public int Id { get; set; }
+        public string Tipo { get; set; }
+        public string Nome { get; set; }
+        public DateTime Validade { get; set; }
+        public decimal Valor { get; set; }
+        public string Descricao { get; set; }
+
+        public bool IsProdutoValido()
+        {
+            return Validade > DateTime.Now;
+        }
+
+        public void AtualizarDataValidade(DateTime novaData)
+        {
+            Validade = novaData;
+        }
+    }
+}
