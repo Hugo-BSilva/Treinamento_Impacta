d6d01789127ee8b073450f560ebebef0a1c14e3c
Hugo Barbosa da Silva <09silvahugo@gmail.com>
2021-09-08 15:52:09 -0300

    Atualizando arquivos

--- Aula-02-POO/EntendendoPOO/EntendendoPOO/Program.cs
+++ Aula-02-POO/EntendendoPOO/EntendendoPOO/Program.cs
@@ -8,6 +8,7 @@ namespace EntendendoPOO
         static void Main(string[] args)
         {
             var validade = new DateTime(2021, 9, 1);
+
             Produto cafe = new Produto()
             {
                 Id = 1,
@@ -16,6 +17,7 @@ namespace EntendendoPOO
                 Tipo = "alimento",
                 Valor = 9.78m
             };
+
             cafe.AtualizarDataValidade(validade);
 
             Produto bolacha = new Produto(3, 
@@ -25,13 +27,24 @@ namespace EntendendoPOO
                 2.89m, 
                 "Bolacha agua e sal");
 
+            Produto leite = new Produto();
+            leite.Nome = "leite";
+            leite.Tipo = "Alimento";
+            leite.Id = 5;
+            leite.Descricao = "leite";
+            leite.Valor = 3.5m;
+            leite.Validade = new DateTime(2021, 9, 1);
+            leite.AtualizarDataValidade(new DateTime(2021, 9, 1));
+
             Usuario usuario = new Usuario() { Id = 1, Nome = "João", Demissao = null };
             Cliente cliente = new Cliente() { Id = 2, Nome = "Luiz", IsMaiorDeIdade= true};
             Pessoa p = new Pessoa() { Id = 3, Nome = "Cleide" };
 
-            List<Pessoa> pessoas = new List<Pessoa>();
-            pessoas.Add(cliente);
-            pessoas.Add(usuario);
+            List<Pessoa> pess = new List<Pessoa>();
+            pess.Add(cliente);
+            pess.Add(usuario);
+
+            Pessoa[] pessoas = new Pessoa[] { cliente, usuario };
 
             foreach (var item in pessoas)
             {
@@ -39,6 +52,11 @@ namespace EntendendoPOO
                 {
                     Console.WriteLine($"Cliente: {item.Nome}, é maior de idade? {((Cliente)item).IsMaiorDeIdade}");
                 }
+            }
+
+            if (!cafe.IsProdutoValido())
+            {
+                Console.WriteLine("Produto não é válido");
             }
             
         }
