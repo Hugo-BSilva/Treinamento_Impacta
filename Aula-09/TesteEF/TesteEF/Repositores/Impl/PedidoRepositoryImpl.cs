2a71cf574113904b68dbf85616d846c440a4a6e1
hugo-bsilva <09silvahugo@gmail.com>
2021-09-21 15:08:56 -0300

    AtualizandoRepositorio

---
+++ Aula-09/TesteEF/TesteEF/Repositores/Impl/PedidoRepositoryImpl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteEF.Repositores.Impl
+{
+    class PedidoRepositoryImpl
+    {
+        public void Salvar(Pedido pedido)
+        {
+            _context.Pedido.Add(pedido);
+            _context.Pedido.SaveChanges()
+        }
+    }
+}
---
+++ Aula-09/TesteEF/TesteEF/Repositores/ProdutoRepository.cs
@@ -0,0 +1,14 @@
+﻿using MaosAObra02.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteEF.Repositores
+{
+    interface ProdutoRepository
+    {
+        List<Produto> ObterProdutos();
+    }
+}
