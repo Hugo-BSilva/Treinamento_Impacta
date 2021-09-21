1b063984868bf19e40fd7aa4e2c8b4a8fac7f3e6
hugo-bsilva <09silvahugo@gmail.com>
2021-09-21 16:56:33 -0300

    Add_RepositoryAndImpl

--- Aula-10/TesteEFP/TesteEFP/Repositores/Impl/ProdutoRepositoryImpl.cs
+++ Aula-10/TesteEFP/TesteEFP/Repositores/Impl/ProdutoRepositoryImpl.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TesteEFP.Models;
 
 namespace TesteEFP.Repositores
 {
-    class ProdutoRepositoryImpl
+    class ProdutoRepositoryImpl : ProdutoRepository
     {
+        private TesteEFPDBContext _context;
+
+        public ProdutoRepositoryImpl(TesteEFPDBContext context)
+        {
+            _context = context;
+        }
+        public List<Produto> ObterProdutos()
+        {
+            // Retorna apenas os produtos que não estão vencidos(vencimento antes da data atual(de hoje))
+            return _context.Produto.Where(produto => produto.Data_Validade > DateTime.Now).ToList();
+        }
+
+        public Produto SelecionarProdutoPorID(int idProduto)
+        {
+            // Trás o primeiro produto que encontrar o id passado
+            return _context.Produto.FirstOrDefault(produto => produto.Id_Produto == idProduto);
+        }
     }
 }
--- Aula-10/TesteEFP/TesteEFP/Repositores/Impl/UsuarioRepositoryImpl.cs
+++ Aula-10/TesteEFP/TesteEFP/Repositores/Impl/UsuarioRepositoryImpl.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TesteEFP.Models;
 
 namespace TesteEFP.Repositores
 {
-    class UsuarioRepositoryImpl
+    class UsuarioRepositoryImpl : UsuarioRepository
     {
+        private TesteEFPDBContext _context;
+
+        public UsuarioRepositoryImpl(TesteEFPDBContext context)
+        {
+            _context = context;
+        }
+
+        public Usuario SelecionarUsuarioID(int idUsuario)
+        {
+            // Retorna o primeiro usuário que encontrar com o ID passado por parâmetro
+            return _context.Usuario.FirstOrDefault(usuario => usuario.Id == idUsuario);
+        }
     }
 }
