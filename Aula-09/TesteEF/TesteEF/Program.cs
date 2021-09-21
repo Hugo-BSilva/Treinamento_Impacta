b53c5133504ff7fafae0f1c240d2d55fbc5a7933
hugo-bsilva <09silvahugo@gmail.com>
2021-09-21 15:11:51 -0300

    AtualizandoProjeot

--- Aula-09/TesteEF/TesteEF/Program.cs
+++ Aula-09/TesteEF/TesteEF/Program.cs
@@ -1,44 +1,13 @@
 ﻿using System;
-using TesteEF.Repositores;
-using TesteEF.Repositores.Impl;
 
 namespace TesteEF
 {
     class Program
     {
-        private static ProdutoRepository _produtoRespository;
+   
         static void Main(string[] args)
         {
-            try
-            {
-                Console.WriteLine("Informe a operação que deseja fazer no banco: \n 1-Listar Produtos \n 2-Criar venda" +
-                    "\n 3-Obter venda por usuário \n 4-Total de itens vendidos do produto");
-                var opcaoStr = Console.ReadLine();
-                var opcao = int.Parse(opcaoStr);
-
-                using (var context = new TesteEFDBContext())
-                {
-                    switch (opcao)
-                    {
-                        case 1:
-                            break;
-                        case 2:
-                            break;
-                        case 3:
-                            break;
-                        case 4:
-                            break;
-                        default:
-                            break;
-                    }
-                }
-                
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
+           
         }
     }
 }
