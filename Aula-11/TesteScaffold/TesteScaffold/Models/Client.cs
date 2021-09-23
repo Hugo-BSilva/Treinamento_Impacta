1def59b1a155cdb2185a5aed31bd530218175031
Hugo Barbosa da Silva <09silvahugo@gmail.com>
2021-09-23 18:06:44 -0300

    TestandoScaffold

---
+++ Aula-11/TesteScaffold/TesteScaffold/Models/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteScaffold.Models
+{
+    [Table("TB_CLIENT")]
+    public class Client
+    {
+        [Key]
+        [Column("ID_CLIENT")]
+        public int Id { get; set; }
+
+        [Column("NAME_CLIENT")]
+        public string NameClient { get; set; }
+
+        [Column("LOCATOR")]
+        public bool Locator { get; set; }
+
+        public virtual ICollection<Movie> Movie { get; set; }
+    }
+}
---
+++ Aula-11/TesteScaffold/TesteScaffold/Models/Movie.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteScaffold.Models
+{
+    [Table("TB_MOVIE")]
+    public class Movie
+    {
+        [Key]
+        [Column("ID_MOVIE")]
+        public int Id { get; set; }
+
+        [Column("ID_CLIENT")]
+        public int IdClient { get; set; }
+
+        [Column("TITLE")]
+        public string Title { get; set; }
+
+        [Column("DATA_RELEASE")]
+        [DataType(DataType.Date)]
+        public DateTime DateRelease { get; set; }
+
+        [Column("GENRE")]
+        public string Genre { get; set; }
+
+        [Column("PRICE", TypeName = "decimal(10,2)")]
+        public decimal Price { get; set; }
+
+        [ForeignKey("IdClient")]
+        public virtual Client Client { get; set; }
+
+    }
+}
