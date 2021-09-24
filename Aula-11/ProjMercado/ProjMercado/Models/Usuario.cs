54f30a08732f6ce05c3a4ea79b67b1fd0a9eb8af
hugo-bsilva <09silvahugo@gmail.com>
2021-09-24 17:21:21 -0300

    Add_ProjetoMercado

---
+++ Aula-11/ProjMercado/ProjMercado/Models/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjMercado.Models
+{
+    
+    class Usuario
+    {
+        
+        public int Id { get; set; }
+
+        
+        public string Nome_Usuario { get; set; }
+
+        
+        public string Data_Nascimento { get; set; }
+
+        //Uma venda tem um usuário, um usuário tem uma lista de vendas
+        public virtual ICollection<Venda> Vendas { get; set; }
+    }
+}
---
+++ Aula-11/ProjMercado/ProjMercado/Models/VendaItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjMercado.Models
+{
+    [Table("TB_VENDA_ITEM")]
+    class VendaItem
+    {
+        [Key]
+        [Column("ID_VENDA_ITEM")]
+        public int Id { get; set; }
+
+        [Column("ID_VENDA")]
+        public int Id_Venda { get; set; }
+
+        [Column("ID_PRODUTO")]
+        public int Id_Produto { get; set; }
+
+        [Column("QUANTIDADE")]
+        public int  Quantidade { get; set; }
+
+
+        // CHAVES ESTRANGEIRAS, DE FORMA RESPECTIVA
+        [ForeignKey("Id_Venda")]
+        public virtual Venda Venda { get; set; }
+        [ForeignKey("Id_Produto")]
+        public virtual Produto Produto { get; set; }
+    }
+}
