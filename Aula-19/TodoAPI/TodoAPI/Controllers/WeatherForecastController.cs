ad760c581fde9c3cf4a81b25d7da0b868b9f4357
Hugo Barbosa da Silva <09silvahugo@gmail.com>
2021-09-30 17:56:29 -0300

    Primeira versao Todo

---
+++ Aula-19/TodoAPI/TodoAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoAPI.Controllers
+{
+    [ApiController] //Não volta tela, volta dados
+    [Route("[controller]")] //Pega o nome do controller e tira o "controller" voltando o weatherForecast
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random(); //Pega número aleatório
+            //Volta a temperatura 
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast //Entre um em cinco
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        public ActionResult CadastrarTemperatura([FromBody] WeatherForecast weather)
+        {
+            return Ok(weather);
+        }
+    }
+}
---
+++ Aula-19/TodoAPI/TodoAPI/Models/TodoAPIDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoAPI.Models
+{
+    public class TodoAPIDBContext : DbContext
+    {
+        public TodoAPIDBContext(DbContextOptions<TodoAPIDBContext> options)
+           : base(options)
+        {
+        }
+        public DbSet<TodoItem> TodoItem { get; set; }
+    }
+}
