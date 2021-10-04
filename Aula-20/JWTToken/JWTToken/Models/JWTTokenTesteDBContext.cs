554cffeaa7cf4723c779d089207814bf20d85825
Hugo Barbosa da Silva <09silvahugo@gmail.com>
2021-10-04 17:54:03 -0300

    Início projeto JWT
    Aprendendo a usar tokens

---
+++ Aula-20/JWTToken/JWTToken/Models/JWTTokenTesteDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTToken.Models
+{
+    public class JWTTokenTesteDBContext : DbContext
+    {
+        private DbSet<Usuario> Usuario { get; set; }
+
+        public JWTTokenTesteDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
